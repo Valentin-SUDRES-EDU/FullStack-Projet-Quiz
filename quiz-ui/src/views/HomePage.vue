b0c570c14d371192b4f10ab58c120d7011b06f43
Valentin SUDRES <valentin.sudres@edu.esiee.fr>
2023-05-20 17:10:12 +0200

    Début de Front

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+import QuizApiService from "@/services/QuizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    var registeredScores = []
+    return {
+      registeredScores
+    };
+  },
+  async created() {
+    console.log("Composant Home page 'created'");
+    registeredScores = QuizApiService.getQuizInfo();
+  }
+};
+</script>
